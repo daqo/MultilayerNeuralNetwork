eb29eabafeb5d9858eef1372129a39be39dd6425
Dave Qorashi <davoud.qorashi@gmail.com>
2014-04-07 01:08:21 -0400

    Adding feture of reading number of attributes and outputs dynamically

--- src/utility.rb
+++ src/utility.rb
@@ -2,13 +2,18 @@ require 'csv'
 class Utility
   def self.read_csv_file(file_path)
     records_on_file = CSV.read(file_path)
-
+    number_of_attributes = records_on_file.shift[0].split(":")[1].to_i
+    number_of_output = records_on_file.shift[0].split(":")[1].to_i
+    
     records = records_on_file.map do |r|
       attributes = r.map { |e| e.to_f }
       correct_format = []
-      correct_format << attributes[0..12]
-      correct_format << attributes[13..15]
+      correct_format << attributes[0..number_of_attributes - 1]
+      correct_format << attributes[number_of_attributes..number_of_attributes + number_of_output]
       correct_format
     end
   end
 end
+
+# correct_format << attributes[0..12]
+# correct_format << attributes[13..15]
