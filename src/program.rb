003af190906bb32037f7156b72cd247cb88b4c8c
Dave Qorashi <davoud.qorashi@gmail.com>
2014-04-07 01:08:21 -0400

    reading number of attirbutes and outputs dynamically

--- src/program.rb
+++ src/program.rb
@@ -2,16 +2,18 @@ require_relative 'utility'
 require_relative 'neural_network'
 
 
-# training_records = Utility.read_csv_file("../data/gameData-normalized.csv")
-# validation_records = Utility.read_csv_file("../data/gameData-validation-set.csv")
+# training_records = Utility.read_csv_file("../data/game/gameData-normalized.csv")
+# validation_records = Utility.read_csv_file("../data/game/gameData-validation-set.csv")
+# multi_layer_neural_network = NeuralNetwork.new(4, 9, 4)
 
-# training_records = Utility.read_csv_file("../data/windsurfData-normalized.csv")
-# validation_records = Utility.read_csv_file("../data/windsurfData-validation-set.csv")
+# training_records = Utility.read_csv_file("../data/windsurf/windsurfData-normalized.csv")
+# validation_records = Utility.read_csv_file("../data/windsurf/windsurfData-validation-set.csv")
+# multi_layer_neural_network = NeuralNetwork.new(4, 4, 1)
 
 training_records = Utility.read_csv_file("../data/wine/wine-revised-normalized.csv")
 validation_records = Utility.read_csv_file("../data/wine/wine-revised-validation-set.csv")
-
 multi_layer_neural_network = NeuralNetwork.new(13, 4, 3)
+
 1000.times do 
 multi_layer_neural_network.train(training_records)
 end
